0d4a56b5a09f0d13cfd0e2f52662b3e399bd384a
ytyasufumi00 <yt.yasufumi00@gmail.com>
2026-06-01 02:04:26 +0000

    クイズ表示変更

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 python -m streamlit run 2_アクション演習.py
 //bash run.sh と入力すればターミナル起動
-// --server.runOnSave true　cntr+cのあとで入力すればポート転送できるpython -m streamlit run Home.py
+// 　cntr+cのあとで入力すればポート転送できるpython -m streamlit run Home.py
