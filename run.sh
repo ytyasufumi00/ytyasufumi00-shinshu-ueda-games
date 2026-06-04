24bd33333bb616476dae88793744f2ffa91acb29
ytyasufumi00 <yt.yasufumi00@gmail.com>
2026-06-04 00:10:18 +0000

    pull

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 python -m streamlit run 2_アクション演習.py
-//bash run.sh と入力すればターミナル起動
-// 　cntr+cのあとで入力すればポート転送できる　python -m streamlit run Home.py
+//bash run.sh と入力すればターミナル起動　python -m streamlit run Home.p
+// 　cntr+cのあとで入力すればポート転送できるy
